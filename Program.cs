d245c9c9fa38ca4fc5e02f5cd0d0f68957fc3d4b
COHL7500 <cohl7500@gmail.com>
2022-09-06 13:35:02 +0200

    Exercise 2 2.0

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+public class Program
+{
+    static void Main(String[] args)
+    {
+        Console.WriteLine("Hello World!");
+    }
+
+    public static bool IsLeapYear(int year)
+    {
+        return year % 4 == 0;
+    }
+}
--- assignment00_test/UnitTest1.cs
+++ assignment00_test/UnitTest1.cs
@@ -5,8 +5,21 @@ namespace assignment00_test;
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void IsLeapYearDivisibleByFourTest()
     {
+        // Arrange
 
+        bool Is_2004_LeapYear;
+        bool Is_4223_LeapYear; 
+
+        // Act
+
+        Is_2004_LeapYear = Program.IsLeapYear(2004);
+        Is_4223_LeapYear = Program.IsLeapYear(4223);
+
+        // Assert
+        
+        Assert.Equal(true, Is_2004_LeapYear);
+        Assert.Equal(false, Is_4223_LeapYear);
     }
 }
